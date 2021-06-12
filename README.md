4fd74001c0ce8a1bee9210cb18e21a5ece6d8d97
Swetha Saseendran <68152189+swetha4444@users.noreply.github.com>
2021-06-13 02:34:36 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-# Resume Analyser Software
-
-## Features
-<ol>
-  <li> Parses the resume (pdf, txt, word file) and retrieves details. </li>
-  <li> Summarises the description and  work-experiences and retrieves key words </li>
-  <li> Batch process a set of resumes and catogerise applicants based on skills and other factors. </li>
-  <li> Data Analysis on batch processed resume </li>
-  <li> Knowledge graph of resume skills and work experience. </li>
-  <li> Resume based job classification </li>
-<ol>
+# Crime-Analysis-Software
---
+++ knowledgeGraph.py
@@ -0,0 +1,87 @@
+# python knowledgeGraph.py
+
+import re
+import pandas as pd
+import bs4
+import requests
+import spacy
+from spacy import displacy
+from spacy.matcher import Matcher 
+from spacy.tokens import Span 
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+
+
+nlp = spacy.load('en_core_web_sm')
+
+#* Get Subject and Object
+def get_entities(sent):
+  ent1 = ""
+  ent2 = ""
+  prv_tok_dep = ""    # dependency tag of previous token in the sentence
+  prv_tok_text = ""   # previous token in the sentence
+  prefix = ""
+  modifier = ""
+  for tok in nlp(sent):
+   if tok.dep_ != "punct":
+      if tok.dep_ == "compound":
+        prefix = tok.text
+        if prv_tok_dep == "compound":
+          prefix = prv_tok_text + " "+ tok.text
+      if tok.dep_.endswith("mod") == True:
+        modifier = tok.text
+        if prv_tok_dep == "compound":
+          modifier = prv_tok_text + " "+ tok.text
+      if tok.dep_.find("subj") == True:
+        ent1 = modifier +" "+ prefix + " "+ tok.text
+        prefix = ""
+        modifier = ""
+        prv_tok_dep = ""
+        prv_tok_text = ""      
+      if tok.dep_.find("obj") == True:
+        ent2 = modifier +" "+ prefix +" "+ tok.text
+      prv_tok_dep = tok.dep_
+      prv_tok_text = tok.text
+  return [ent1.strip(), ent2.strip()]
+
+#* Get Verb
+def get_relation(sent):
+  doc = nlp(sent)
+  # Matcher class object 
+  matcher = Matcher(nlp.vocab)
+  #define the pattern 
+  pattern = [{'DEP':'ROOT'}, 
+            {'DEP':'prep','OP':"?"},
+            {'DEP':'agent','OP':"?"},  
+            {'POS':'ADJ','OP':"?"}] 
+  matcher.add("matching_1",[pattern]) 
+  matches = matcher(doc)
+  k = len(matches) - 1
+  span = doc[matches[k][1]:matches[k][2]] 
+  return(span.text)
+
+def create_graph(source, target, relation,kg_df):
+    G=nx.from_pandas_edgelist(kg_df, "source", "target", 
+                          edge_attr=True, create_using=nx.MultiDiGraph())
+    plt.figure(figsize=(12,12))
+    pos = nx.spring_layout(G)
+    nx.draw(G, with_labels=True, node_color='skyblue', edge_cmap=plt.cm.Blues, pos = pos)
+    nx.draw_networkx_edge_labels(G,pos,edge_labels={(source,target):relation},font_color='red')
+    plt.axis('off')
+    plt.show()
+
+
+
+
+'''
+entity_pairs = get_entities("John completed the task")
+verb = get_relation("John completed the task")
+print(entity_pairs,verb)
+source = entity_pairs[0]
+target = entity_pairs[1]
+relation = verb
+kg_df = pd.DataFrame({'source':[source], 'target':[target], 'edge':[relation]})
+create_graph(source,target,relation,kg_df)
+'''
